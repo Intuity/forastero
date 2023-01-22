069389fdf03d8891c7bf499a67ff79f1d5229d98
Peter Birch <peter@lightlogic.co.uk>
2023-01-22 17:31:56 +0000

    Initial code

---
+++ forastero/bench.py
@@ -0,0 +1,187 @@
+# Copyright 2021, Peter Birch, mailto:peter@lightlogic.co.uk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+import os
+from typing import Any
+
+import cocotb
+from cocotb.triggers import ClockCycles
+from cocotb.utils import get_sim_time
+from cocotb_bus.scoreboard import Scoreboard
+from cocotb_bus.drivers import Driver
+
+from .driver import BaseDriver
+from .monitor import BaseMonitor
+
+class BaseBench:
+
+    def __init__(self, dut, clk="i_clk", rst="i_rst"):
+        """ Initialise the base testbench.
+
+        Args:
+            dut: Pointer to the DUT
+        """
+        # Hold a pointer to the DUT
+        self.dut = dut
+        # Promote clock & reset
+        self.clk = getattr(dut, clk)
+        self.rst = getattr(dut, rst)
+        # Expose logging methods
+        self.debug   = dut._log.debug
+        self.info    = dut._log.info
+        self.warning = dut._log.warning
+        self.error   = dut._log.error
+        # Create a scoreboard
+        imm_fail = (os.environ.get("FAIL_IMMEDIATELY", "no").lower() == "yes")
+        self.scoreboard = Scoreboard(self, fail_immediately=imm_fail)
+        # Track drivers and monitors
+        self.drivers  = {}
+        self.monitors = {}
+
+    async def initialise(self):
+        """ Initialise the DUT's I/O """
+        self.rst.value = 1
+
+    async def reset(self, init=True, wait=20):
+        """ Reset the DUT.
+
+        Args:
+            init: Initialise the DUT's I/O
+            wait: Number of clock cycles to wait after init and reset
+        """
+        # Drive reset high
+        self.rst.value = 1
+        # Initialise I/O
+        if init:
+            await self.initialise()
+            await ClockCycles(self.clk, wait)
+        # Drop reset
+        self.rst.value = 0
+        # Wait for a bit
+        await ClockCycles(self.clk, wait)
+
+    def __getattr__(self, key):
+        """ Pass through accesses to signals on the DUT.
+
+        Args:
+            key: Name of the attribute
+        """
+        try:
+            return getattr(super(), key)
+        except Exception:
+            return getattr(self.dut, key)
+
+    def register_driver(self, name : str, inst : Driver) -> None:
+        """
+        Register a driver with the testbench, will be included in the shutdown
+        handling.
+
+        Args:
+            name: Name of the driver
+            inst: Instance of the driver
+        """
+        assert isinstance(inst, BaseDriver), "Not a subclass of BaseDriver"
+        self.drivers[name] = inst
+        setattr(self, name, inst)
+
+    def register_monitor(self, name : str, inst : BaseMonitor) -> None:
+        """
+        Register a monitor with the testbench, creating an expected transaction
+        list and linking it to the scoreboard.
+
+        Args:
+            name: Name of the monitor to register
+            inst: Instance of the monitor
+        """
+        assert isinstance(inst, BaseMonitor), "Not a subclass of BaseMonitor"
+        self.monitors[name] = inst
+        setattr(self, name, inst)
+        def _compare_func(got : Any):
+            return self.__compare_transactions(inst, got)
+        self.scoreboard.add_interface(inst,
+                                      inst.expected,
+                                      compare_fn=_compare_func)
+
+    def __compare_transactions(self, monitor : BaseMonitor, got : Any) -> None:
+        """
+        Check that the received transaction of a monitor and the next expected
+        transaction in the monitor's queue match one another, and verbosely
+        print out the differences when an error occurs.
+
+        Args:
+            monitor: Pointer to the monitor which collected the transaction
+            got    : Transaction received by the monitor
+        """
+        # Pop the next expected transaction
+        exp     = monitor.expected.pop(0)
+        fmt_int = lambda x: (hex(x) if not isinstance(x, Enum) and
+                                       isinstance(x, int) and
+                                       x > 9 else str(x))
+        # Check to see expected transaction matches received data
+        # NOTE: Monitor's may provide a 'compare' method to override '!='
+        if (
+            (monitor.compare is     None and exp != got                   ) or
+            (monitor.compare is not None and not monitor.compare(got, exp))
+        ):
+            self.error(
+                f"Unexpected {type(exp).__name__} received by {monitor.name} at "
+                f"{get_sim_time('ns')} ns:"
+            )
+            max_key = max((len(x) for x in vars(exp).keys()))
+            entries = []
+            for key, exp_val in vars(exp).items():
+                got_val = getattr(got, key)
+                exp_str = fmt_int(exp_val)
+                got_str = fmt_int(got_val)
+                entries.append((key, exp_str, got_str))
+            max_key = max((len(x[0]) for x in entries))
+            max_exp = max((len(x[1]) for x in entries))
+            max_got = max((len(x[2]) for x in entries))
+            for key, exp, got in entries:
+                self.info(
+                    f" - [{[' ','!'][exp != got]}] {key:<{max_key}s} - "
+                    f"E: {exp:<{max_exp}s}, G: {got:<{max_got}s}"
+                )
+            self.scoreboard.errors += 1
+            if self.scoreboard._imm:
+                assert self.scoreboard.errors == 0
+
+    async def close_down(self) -> None:
+        """ Wait for all drivers and monitors to drain """
+        # Wait for all drivers to return to idle
+        for key, driver in self.drivers.items():
+            self.info(f"Waiting for driver '{key}' to go idle")
+            await driver.idle()
+        # Wait for all monitor queues to drain
+        for key, monitor in self.monitors.items():
+            self.info(f"Waiting for monitor '{key}' to go idle")
+            await monitor.idle()
+
+    @classmethod
+    def testcase(cls, *args, **kwargs) -> None:
+        """ Custom testcase declaration, wraps test with bench class"""
+        class _testcase(cocotb.test):
+            def __call__(self, dut, *args, **kwargs):
+                async def __run_test():
+                    tb = cls(dut)
+                    await self._func(tb, *args, **kwargs)
+                    await tb.close_down()
+                    raise tb.scoreboard.result
+                return cocotb.decorators.RunningTest(__run_test(), self)
+        def _do_decorate(func):
+            # _testcase acts as a function which returns a decorator, hence the
+            # double function call
+            return _testcase(*args, **kwargs)(func)
+        return _do_decorate
---
+++ forastero/io.py
@@ -0,0 +1,90 @@
+# Copyright 2021, Peter Birch, mailto:peter@lightlogic.co.uk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import IntEnum
+
+class IORole(IntEnum):
+    """ Role that a particular bus is performing (determines signal suffix) """
+    INITIATOR = 0
+    RESPONDER = 1
+
+class BaseIO:
+    """ Base I/O wrapper class """
+
+    def __init__(self, dut, name, role, init_sigs, resp_sigs):
+        """ Initialise BaseIO.
+
+        Args:
+            dut      : Pointer to the DUT boundary
+            name     : Name of the signal - acts as a prefix
+            role     : Role of this signal on the DUT boundary
+            init_sigs: Signals driven by the initiator
+            resp_sigs: Signals driven by the responder
+        """
+        # Sanity checks
+        assert role in IORole, f"Role {role} is not recognised"
+        assert isinstance(init_sigs, list), "Initiator signals are not a list"
+        assert isinstance(resp_sigs, list), "Responder signals are not a list"
+        # Hold onto attributes
+        self.__dut       = dut
+        self.__name      = name
+        self.__role      = role
+        self.__init_sigs = init_sigs[:]
+        self.__resp_sigs = resp_sigs[:]
+        # Pickup attributes
+        self.__initiators, self.__responders = {}, {}
+        for comp in self.__init_sigs:
+            sig  = "o" if self.__role == IORole.INITIATOR else "i"
+            sig += f"_{self.__name}_{comp}"
+            if not hasattr(self.__dut, sig):
+                print(f"{type(self).__name__}: Did not find I/O component {sig} on {dut}")
+                continue
+            sig_ptr = getattr(self.__dut, sig)
+            self.__initiators[comp] = sig_ptr
+            setattr(self, comp, sig_ptr)
+        for comp in self.__resp_sigs:
+            sig  = "i" if self.__role == IORole.INITIATOR else "o"
+            sig += f"_{self.__name}_{comp}"
+            if not hasattr(self.__dut, sig):
+                print(f"{type(self).__name__}: Did not find I/O component {sig} on {dut}")
+                continue
+            sig_ptr = getattr(self.__dut, sig)
+            self.__responders[comp] = sig_ptr
+            setattr(self, comp, sig_ptr)
+
+    def initialise(self, role):
+        """ Initialise signals according to the active role """
+        for sig in (
+            self.__initiators if role == IORole.INITIATOR else self.__responders
+        ).values():
+            sig.value = 0
+
+    def has(self, comp):
+        return (comp in self.__initiators) or (comp in self.__responders)
+
+    def get(self, comp, default=None):
+        item = getattr(self, comp, None)
+        if item is None:
+            return default
+        else:
+            raw = int(item.value)
+            return (raw == 1) if len(item) == 1 else raw
+
+    def set(self, comp, value):
+        if not self.has(comp): return
+        getattr(self, comp).value = value
+
+    def width(self, comp):
+        sig = getattr(self, comp)
+        return max(sig._range) - min(sig._range) + 1
